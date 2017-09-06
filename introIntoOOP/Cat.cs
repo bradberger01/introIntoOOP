6fdb92b492a07b402d1ffb41fb468f9a752969c2
bradberger01 <bradberger@hotmail.com>
2017-09-06 19:35:22 -0400

    More OOP

---
+++ introIntoOOP/Cat.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introIntoOOP
+{
+    class Cat
+    {//Fields
+        private string name;
+        private string furColor;
+        private int furLength;
+        private bool isMale;
+        private int age;
+        private bool isHungry;
+    //Properties (like the middleman to get to the private fields)
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        //Constructors
+
+        public Cat()
+        {
+            this.name = "Mittens";
+        }
+        public Cat(string name)
+        {
+            this.name = name;
+        }
+        public Cat(int age)
+        {
+            this.age = age;
+        }
+        public Cat(string name, string furColor, int furLength, int age, bool isMale)
+        {
+            this.name = name;
+            this.furColor = furColor;
+            this.furLength = furLength;
+            this.age = age;
+            this.isMale = isMale;
+
+        }
+        public int Age
+        {
+            get { return this.age; }
+        }
+    }
+}
--- introIntoOOP/Program.cs
+++ introIntoOOP/Program.cs
@@ -10,6 +10,14 @@ namespace introIntoOOP
     {
         static void Main(string[] args)
         {
+            Cat cat1 = new Cat();
+            Cat cat2 = new Cat("Neko", "black", 3, 1, true);
+            Console.WriteLine(cat1.Name);
+            Console.WriteLine(cat2.Name);
+            cat1.Name = "Gato";
+            Console.WriteLine(cat1.Name);
+            Console.WriteLine(cat2.Age);
+            Console.WriteLine(cat1.Age);
         }
     }
 }
